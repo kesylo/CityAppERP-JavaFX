f4ad2a00d4edd2c7c0b21b5874b96c213c68f985
kesylo <keming.loic@gmail.com>
2020-02-03 15:21:51 +0100

    another fix for time computation with more precision using milliseconds

--- src/sample/Database/DBConfig.java
+++ src/sample/Database/DBConfig.java
@@ -2,7 +2,7 @@ package sample.Database;
 
 class DBConfig {
     // app version
-    private static String appVersion = "3.2.1";
+    private static String appVersion = "3.2.3";
 
     // my local Server
     /*String dbHost = "localhost";
